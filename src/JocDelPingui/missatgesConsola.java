5c6bb19ebc5edf489f717063bb3046b037e24805
didacsantolaria-code <didacsantolaria@alumnes.ilerna.com>
2026-02-26 16:57:56 +0100

    Mensaje de bienvenida (missatgesConsola)

--- src/JocDelPingui/missatgesConsola.java
+++ src/JocDelPingui/missatgesConsola.java
@@ -1,5 +1,10 @@
 package JocDelPingui;
 
 public class missatgesConsola {
-
+	public static void mostrarBienvenida() {
+        System.out.println("\n╔════════════════════════════════════╗");
+        System.out.println("║     BIENVENIDO AL JUEGO PENGUIN    ║");
+        System.out.println("╚════════════════════════════════════╝");
+        System.out.println();
+    }
 }
