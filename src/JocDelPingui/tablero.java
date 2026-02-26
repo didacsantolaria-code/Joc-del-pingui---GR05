975b624daf7322bd3f5559ccf322ff09fc98bdc9
didacsantolaria-code <didacsantolaria@alumnes.ilerna.com>
2026-02-26 16:48:02 +0100

    Update tablero.java

--- src/JocDelPingui/tablero.java
+++ src/JocDelPingui/tablero.java
@@ -1,5 +1,21 @@
 package JocDelPingui;
 
-public class tablero {
+import java.util.Random;
 
+public class tablero {
+	 private int[] casillas;
+	 private String nombre;
+	 private Random random;
+	 
+	 public tablero() {
+		 this.nombre = "Tablero Principal";
+		 this.casillas = new int[50];
+		 this.random = new Random();
+	 }
+	 
+	 public tablero(String nombre) {
+		 this.nombre = nombre;
+		 this.casillas = new int[50];
+		 this.random = new Random();
+	 }
 }
