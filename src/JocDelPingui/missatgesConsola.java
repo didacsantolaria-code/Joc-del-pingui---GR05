17697e2e99a839056c28fb773a99a222481862b6
didacsantolaria-code <didacsantolaria@alumnes.ilerna.com>
2026-02-27 15:56:15 +0100

    Cambio de mensajes
    Modificado mensaje de mostrarBienvenida() y quitado comentario de class pingui

--- src/JocDelPingui/missatgesConsola.java
+++ src/JocDelPingui/missatgesConsola.java
@@ -3,7 +3,7 @@ package JocDelPingui;
 public class missatgesConsola {
 	public static void mostrarBienvenida() {
         System.out.println("\n╔════════════════════════════════════╗");
-        System.out.println("║     BIENVENIDO AL JUEGO PENGUIN    ║");
+        System.out.println("║     BIENVENIDO AL JUEGO DEL PINGUI ║");
         System.out.println("╚════════════════════════════════════╝");
         System.out.println();
     }
