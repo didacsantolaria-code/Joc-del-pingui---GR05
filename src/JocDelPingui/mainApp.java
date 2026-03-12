3de409262320645492ea34749377ddf7dce3bd93
didacsantolaria-code <didacsantolaria@alumnes.ilerna.com>
2026-03-12 17:24:42 +0100

    Update mainApp.java

--- src/JocDelPingui/mainApp.java
+++ src/JocDelPingui/mainApp.java
@@ -1,21 +1,14 @@
 package JocDelPingui;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
+public class mainApp extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("🐧 JavaFX funciona! 🐧");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 400, 300);
-        
-        primaryStage.setTitle("Juego del Pingüino");
-        primaryStage.setScene(scene);
+        System.out.println("JavaFX funciona!");
+        primaryStage.setTitle("Ventana vacía");
         primaryStage.show();
     }
     
